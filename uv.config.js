e286ec3cda652cb8114ae493d9145fd7e3979377
MinsUR4 <164434993+MinsUR4@users.noreply.github.com>
2024-03-22 16:33:57 -0400

    Create uv.config.js

---
+++ uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/astronomy/',
+    bare: '/outerspace/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: 'https://cdn.jsdelivr.net/gh/MinsUR4/Loading@main/uv.handler.js',
+    bundle: 'https://cdn.jsdelivr.net/gh/MinsUR4/Loading@main/uv.bundle.js',
+    config: ''https://cdn.jsdelivr.net/gh/MinsUR4/Interstellar@main/uv.config.js',
+    sw: 'https://cdn.jsdelivr.net/gh/MinsUR4/Interstellar@main/uv.sw.js',
+};
